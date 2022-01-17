3f1fe44772d1091e08bb82d372129c7c57816ef2
刘文杰 <liuwenjie123007@qq.com>
2022-01-17 23:54:12 +0900

    私有性:

--- src/main/java/gitbub/com/liuwenjie1234007/tdd/ch1/Dollar.java
+++ src/main/java/gitbub/com/liuwenjie1234007/tdd/ch1/Dollar.java
@@ -5,7 +5,7 @@ package gitbub.com.liuwenjie1234007.tdd.ch1;
  * @since 1.0.0
  **/
 public class Dollar {
-    int amount;
+    private final int amount;
 
     Dollar(int amount) {
         this.amount = amount;
