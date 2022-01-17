cc0f3e8182aefbbf02846eae8ea791415393d6d8
刘文杰 <liuwenjie123007@qq.com>
2022-01-18 00:02:42 +0900

    法郎在诉说

---
+++ src/main/java/gitbub/com/liuwenjie1234007/tdd/ch1/Franc.java
@@ -0,0 +1,21 @@
+package gitbub.com.liuwenjie1234007.tdd.ch1;
+
+/**
+ * @author liuwenjie
+ * @since 1.0.0
+ **/
+public class Franc {
+    private int amount;
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
