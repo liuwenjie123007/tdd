a8d2e3b602aa8d72ad684d8e4b812aa8d6f0d7c9
刘文杰 <liuwenjie123007@qq.com>
2022-01-19 23:17:51 +0900

    设置表格

--- part2-python/main.py
+++ part2-python/main.py
@@ -2,27 +2,42 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
+        self.wasSetUp = None
+        self.wasRun = None
 
     def testMethod(self):
         self.wasRun = 1
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 if __name__ == '__main__':
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
