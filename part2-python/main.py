a40bd4a1ef78a1fdbf4311fe469236c6eb9a3f18
刘文杰 <liuwenjie123007@qq.com>
2022-01-19 22:59:02 +0900

    步入xUnit

--- part2-python/main.py
+++ part2-python/main.py
@@ -1,17 +1,28 @@
-# coding=utf-8
-# 这是一个示例 Python 脚本。
+class TestCase:
+    def __init__(self, name):
+        self.name = name
 
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print("Hi, {0}".format(name))  # 按 ⌘F8 切换断点。
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
 
-# 按间距中的绿色按钮以运行脚本。
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert (not test.wasRun)
+        test.run()
+        assert test.wasRun
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    TestCaseTest("testRunning").run()
