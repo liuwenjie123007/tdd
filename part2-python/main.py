22c12bfed4637ff5d4832cfe8baef5c827422f8f
刘文杰 <liuwenjie123007@qq.com>
2022-01-21 21:47:16 +0900

    失败处理

--- part2-python/main.py
+++ part2-python/main.py
@@ -9,8 +9,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
 
@@ -39,12 +42,16 @@ class WasRun(TestCase):
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
 
 class TestCaseTest(TestCase):
@@ -70,7 +77,12 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert ("1 run, 1 failed", result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert ("1 run, 1 failed" == result.summary())
+
 
 if __name__ == '__main__':
-    TestCaseTest("testRunning").run()
     TestCaseTest("testSetUp").run()
